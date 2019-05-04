34a44e383d34aa4d4c0923225ae5039c16549dfe
leilvlong <1455013965@qq.com>
2019-05-04 15:04:22 +0800

    修改

--- TomCatServlet/demo02/src/com/tomcatservlet/jdbcutil/LoginUtil.java
+++ TomCatServlet/demo02/src/com/tomcatservlet/jdbcutil/LoginUtil.java
@@ -1,7 +1,7 @@
 package com.tomcatservlet.jdbcutil;
 
 import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class LoginUtil {
     public static Cookie getCookie(String key,String value, int setMaxAge, String path){
@@ -9,6 +9,13 @@ public class LoginUtil {
         cookie.setMaxAge(7*24*60*60);
         cookie.setPath(path);
         return cookie;
+    }
+
+    public static void addCookie(HttpServletResponse response, String key, String value, int setMaxAge, String path){
+        Cookie cookie = new Cookie(key,value);
+        cookie.setMaxAge(7*24*60*60);
+        cookie.setPath(path);
+        response.addCookie(cookie);
     }
 
 
